bf5463649edb5d7920fcdf35b7b7a8b4a3c59fed
adel <adel.albediwy@gmail.com>
2022-11-29 19:54:34 +0300

    updated docker

--- src/main/java/com/adel/sslspring/Controller.java
+++ src/main/java/com/adel/sslspring/Controller.java
@@ -3,13 +3,15 @@ package com.adel.sslspring;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @Slf4j
 @RestController
+@RequestMapping("/api/v1")
 public class Controller {
-    @GetMapping
+    @GetMapping("/hit")
     public void hit(){
         log.info("GOT HIT");
     }
